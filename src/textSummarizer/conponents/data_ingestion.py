60e1885d1c8afde32cd585b98265b7a1ed6ca5f1
satwik <pasumarthipsatwik@gmail.com>
2024-06-26 11:13:01 +0530

    data transformation completed

--- src/textSummarizer/conponents/data_ingestion.py
+++ src/textSummarizer/conponents/data_ingestion.py
@@ -18,7 +18,7 @@ class DataIngestion:
             )
             logger.info(f"{filename} download! with following info: \n{headers}")
         else:
-            logger.ingo(f"file already exists of size: {get_size(Path(self.config.local_data_file))}")
+            logger.info(f"file already exists of size: {get_size(Path(self.config.local_data_file))}")
             
     def extract_zip_file(self):
         unzip_path = self.config.unzip_dir
