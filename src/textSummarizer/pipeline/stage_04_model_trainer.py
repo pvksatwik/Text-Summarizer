584ea0eff8d8087192af650cdc4e231b9724c8c3
satwik <pasumarthipsatwik@gmail.com>
2024-06-26 16:34:30 +0530

    model evaluation completed

---
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,13 @@
+from textSummarizer.config.configuration import configurationManager
+from textSummarizer.conponents.model_trainer import ModelTrainer
+from textSummarizer.logging import logger
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = configurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
